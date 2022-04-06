c80acaa6a6b95d28bbb4096b2556d292965a9532
korn1evski <corn1evski@gmail.com>
2022-04-06 13:25:10 +0300

    initial setup

---
+++ mytube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mytube
+//
+//  Created by Bogdan Kornievski on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
